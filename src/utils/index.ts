38843bec3799e60b99b724ff4a7d948e59447ce5
Wei Li <ng_wei_li@tech.gov.sg>
2023-01-06 10:53:46 +0800

    [IMAGEUPLOAD][WL] add events to image-upload - add events - replace lifesg-specific logic with events - remove lifesg-specific codes

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -3,5 +3,4 @@ export * from "./date-time-helper";
 export * from "./file-helper";
 export * from "./image-helper";
 export * from "./test-helper";
-export * from "./webview-helper";
 export * from "./window-helper";
